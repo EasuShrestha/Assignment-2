4c0aa3946b898a0d9dba52508ffed0cb5c2bc1f6
EasuShrestha <easushrstha331@gmail.com>
2024-08-03 21:14:00 +0545

    Assignment-2

---
+++ Function/function.c
@@ -0,0 +1,33 @@
+/*Function is a block of code that performs particular task
+It take argument, Do work and return result
+It can be used multiple times.
+It increase code reusablity.
+
+Syntax-1
+Function Prototype/declaration.
+void printhello();
+
+Syntax-2
+Function Definition
+void printHello(){
+printf("Hello");
+}
+
+Syntax-3
+Function call
+int main(){
+printHello();
+return 0;
+}
+*/
+//Example
+#include<stdio.h>
+void hello();
+int main();
+{
+    print My name is easu();
+return 0;
+}
+void hello(){
+    printf("My name is easu");
+}
